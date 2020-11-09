99a19c93dc8488b271412dd9b309b55a535cddc4
Gabriel Lima <gabriel.francisco65@yahoo.com>
2020-11-09 18:40:10 -0300

    .

---
+++ menu.js
@@ -0,0 +1,50 @@
+document.write(`
+<header>
+<nav id='cssmenu'>
+  <div class="logo"><a href="index.html"><img src="/imagens/Logobeta2.png" width="40px" height="23.5px">Grand Turismo</a></div>
+  <div id="head-mobile"></div>
+  <div class="button"></div>
+  <ul>
+    <li class='active'><a href='#'>HOME</a></li>
+    <li><a href='#'>SOBRE</a></li>
+    <li><a href='#'>LOCAIS</a>
+      <ul>
+        <li><a href='#'>ARQUITETURAS E PAISAGENS</a>
+          <ul>
+            <li><a href='/conteudohtml/arquipaisagem/catedral/catedral.html'>CATEDRAL</a></li>
+            <li><a href='#'>Sub Product</a></li>
+          </ul>
+        </li>
+        <li><a href='#'>MUSEUS</a>
+          <ul>
+            <li><a href='#'>Sub Product</a></li>
+            <li><a href='#'>Sub Product</a></li>
+          </ul>
+        </li>
+        <li><a href='#'>PRAÇAS</a>
+          <ul>
+            <li><a href='#'>Sub Product</a></li>
+            <li><a href='#'>Sub Product</a></li>
+          </ul>
+        </li>
+        <li><a href='#'>PRAIAS</a>
+          <ul>
+            <li><a href='#'>Sub Product</a></li>
+            <li><a href='#'>Sub Product</a></li>
+          </ul>
+        </li>
+        <li><a href='#'>RESTAURANTES</a>
+          <ul>
+            <li><a href='#'>Sub Product</a></li>
+            <li><a href='#'>Sub Product</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li><a href='#'>MULTIMIDIAS</a></li>
+    <li><a href='#'>BLOG</a></li>
+    <li><a href='#'>CONTATO</a></li>
+  </ul>
+</nav>
+</header>
+`);
